410706eca75b4e11b12a0fb3228b9e8ba6368886
Lakshya Shastri <lakshyashastri11@gmail.com>
2020-11-17 01:24:22 +0530

    merge edits and stuff

--- student/options_student.py
+++ student/options_student.py
@@ -2,6 +2,3 @@ first_prompt = {
     1: "Create student ID",
     2: "Login with existing credentials"
 }
-
-
-
--- student/student.py
+++ student/student.py
@@ -1,92 +1,62 @@
 import os
 import MySQLdb
 
-
-from student.helpers_student import *
-from student.options_student import *
-from student.sql_functions_student import *
-
-
+from helpers_student import *
+from options_student import *
+from sql_functions_student import *
 
 db = MySQLdb.connect(host = "localhost", user = "root", passwd = os.environ['sqlpwd'])
 cursor = db.cursor()
 
-
-
-
 #table name template = "student_id--student_pw"
 
-
-
-qwe("SHOW DATABASES")
+cursor.execute("SHOW DATABASES")
 if ('students', ) not in cursor.fetchall():
-    qwe("CREATE DATABASE students")
+    cursor.execute("CREATE DATABASE students")
     db.commit()
-
-
-
 
 #login/sign_up process
 while True:
-
-    qwe("USE students")
+    cursor.execute("USE students")
     display_options(first_prompt)
 
-    while True:
+    first_choice = get_choice(first_prompt)
 
-        #login/sign-up?
-        first_choice = get_choice(first_prompt)
+    if first_choice == 1:
 
-        #sign-up
-        if first_choice == 1:
+        while True:
+            new_id = input("Enter a test username: ")
 
-
-            new_id = input("Enter a unique username: ")
-            qwe("SHOW TABLES")
+            cursor.execute("SHOW TABLES")
             for table_name in cursor.fetchall():
                 if table_name.split(" | ")[0] == new_id:
-                    print("This username has already been taken. Please choose a different username.")
+                    print("This username has already been taken. Please choose a different username\n")
                     break
-
-
-            new_pw = input("Enter password: ")
-            new_pw_confirm = input("Confirm password: ")
-
-            if new_pw != new_pw_confirm:
-                print("The entered passwords do not match. Please try again.")
+            else:
                 break
-            
-
-            # still have to add table columns. Might add datetime to store when their acc/ID was created.
-            qwe(f"CREATE TABLE {get_table_name(new_id,new_pw)}")
-            print("New ID created. Returning to the previous page.")
-            break
-        
-
-        #login
-        elif first_choice == 2:
-
-
-            login_id = input("Enter your student ID: ")
-            login_pw = input("Enter your password: ")
-
-
-            qwe("SHOW TABLES")
-            for table_name in cursor.fetchall():
-
-                if get_table_name(login_id,login_pw) == table_name:
-                    print("Logged in successfully.")
-                    break
                 
-                else:
-                    print("The entered credentials may be wrong. Please try again.")
-                    break
+        new_pw = confirm_password()
+
+        # still have to add table columns. Might add datetime to store when their acc/ID was created.
+        cursor.execute(f"CREATE TABLE {get_table_name(new_id, new_pw)}")
+        print("New ID created")
+        logged_in = True
+
+    else:
+        login_id = input("Enter your student ID: ")
+        login_pw = input("Enter your password: ")
+
+        cursor.execute("SHOW TABLES")
+        for table_name in cursor.fetchall():
+            if get_table_name(login_id,login_pw) == table_name:
+                print("Logged in successfully")
+                logged_in = True
+        else:
+            print("The entered credentials may be wrong or you might not have an account.")
+            logged_in = False
+    
+    if logged_in:
         break
-    break
-
-
-
-
 
 #WIP
 while True:
@@ -94,10 +64,9 @@ while True:
     while True:
 
         #displaying available tests and getting a test choice for them to attempt
-
         display_options(get_test_name_dict())
-
         get_choice(get_test_name_dict())
+
 
         
 
