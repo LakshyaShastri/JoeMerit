cef9eebd430b372cb772f493346e74b1979e314f
Afraz Khan <60065049+Afraz0Khan@users.noreply.github.com>
2020-11-24 01:24:16 +0530

    Update admin/admin.py
    suggested commit (da duble tuple ting)
    
    Co-authored-by: Lakshya Shastri <62901541+LakshyaShastri@users.noreply.github.com>

--- admin/admin.py
+++ admin/admin.py
@@ -162,7 +162,7 @@ while True:
             #checking if the test exists
             cursor.execute("USE admin")
             cursor.execute("SHOW TABLES")
-            if test_name not in cursor.fetchall()[0]:
+            if (test_name, ) not in cursor.fetchall():
                 print("The entered test name does not exist. Please enter a valid test.")
                 break
 
@@ -193,7 +193,6 @@ while True:
 
             elif cont.lower() == "n":
                 break
-
 
 
 
